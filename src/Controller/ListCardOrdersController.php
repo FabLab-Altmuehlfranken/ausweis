c7bad9e49649c3297041a895ef86722ea648d7b9
Bernd Stellwag <burned@zerties.org>
2026-02-14 18:44:57 +0100

    fix rector

--- src/Controller/ListCardOrdersController.php
+++ src/Controller/ListCardOrdersController.php
@@ -13,11 +13,16 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class ListCardOrdersController extends AbstractController
 {
+    public function __construct(
+        private readonly CardOrderRepository $repository,
+    ) {
+    }
+
     #[Route('/card_orders', name: 'list_card_orders')]
     #[IsGranted(User::ADMIN_ROLE)]
-    public function index(CardOrderRepository $repository): Response
+    public function index(): Response
     {
-        $orders = $repository->findAll();
+        $orders = $this->repository->findAll();
 
         return $this->render('list_card_orders/index.html.twig', [
             'orders' => $orders,
