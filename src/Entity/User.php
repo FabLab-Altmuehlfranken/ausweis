5bf1cc7f92c0cc7c8a984057ce434026dc928a6b
Bernd Stellwag <burned@zerties.org>
2026-02-23 22:57:55 +0100

    remove unused use statement

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Deprecated;
 use Doctrine\ORM\Mapping as ORM;
 use Override;
 use Symfony\Bridge\Doctrine\Types\UuidType;
