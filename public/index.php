246879ba8a40956141c1e107dc1a6cbff07b8256
Bernd Stellwag <burned@zerties.org>
2026-04-21 21:38:45 +0200

    update symfony/framework-bundle recipe

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-return function (array $context) {
+return static function (array $context) {
     assert(is_string($context['APP_ENV']));
 
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
