2cc08a083f6b0197f0328248fc8daa02a1dac125
Bernd Stellwag <burned@zerties.org>
2026-09-22 23:57:52 +0200

    use make:crud to create basic crud functionality for privileges

---
+++ src/Controller/PrivilegeController.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Privilege;
+use App\Entity\User;
+use App\Form\PrivilegeType;
+use App\Repository\PrivilegeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/privilege')]
+#[IsGranted(User::ADMIN_ROLE)]
+final class PrivilegeController extends AbstractController
+{
+    #[Route(name: 'app_privilege_index', methods: ['GET'])]
+    public function index(PrivilegeRepository $privilegeRepository): Response
+    {
+        return $this->render('privilege/index.html.twig', [
+            'privileges' => $privilegeRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_privilege_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $privilege = new Privilege();
+        $form = $this->createForm(PrivilegeType::class, $privilege);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($privilege);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_privilege_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('privilege/new.html.twig', [
+            'privilege' => $privilege,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_privilege_show', methods: ['GET'])]
+    public function show(Privilege $privilege): Response
+    {
+        return $this->render('privilege/show.html.twig', [
+            'privilege' => $privilege,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_privilege_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Privilege $privilege, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PrivilegeType::class, $privilege);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_privilege_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('privilege/edit.html.twig', [
+            'privilege' => $privilege,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_privilege_delete', methods: ['POST'])]
+    public function delete(Request $request, Privilege $privilege, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$privilege->id, $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($privilege);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_privilege_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/PrivilegeType.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Entity\Area;
+use App\Entity\Privilege;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @template-extends AbstractType<Privilege>
+ */
+class PrivilegeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('area', EntityType::class, [
+                'class' => Area::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Privilege::class,
+        ]);
+    }
+}
