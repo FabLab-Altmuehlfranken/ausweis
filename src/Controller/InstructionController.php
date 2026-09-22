28db303bb56e387bff1434b5d0681c8a3262eb85
Bernd Stellwag <burned@zerties.org>
2026-09-22 23:58:02 +0200

    use make:crud to create basic crud functionality for instructions

---
+++ src/Controller/InstructionController.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\Instruction;
+use App\Entity\User;
+use App\Form\InstructionType;
+use App\Repository\InstructionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/instruction')]
+#[IsGranted(User::ADMIN_ROLE)]
+final class InstructionController extends AbstractController
+{
+    #[Route(name: 'app_instruction_index', methods: ['GET'])]
+    public function index(InstructionRepository $instructionRepository): Response
+    {
+        return $this->render('instruction/index.html.twig', [
+            'instructions' => $instructionRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_instruction_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $instruction = new Instruction();
+        $form = $this->createForm(InstructionType::class, $instruction);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($instruction);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_instruction_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('instruction/new.html.twig', [
+            'instruction' => $instruction,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_instruction_show', methods: ['GET'])]
+    public function show(Instruction $instruction): Response
+    {
+        return $this->render('instruction/show.html.twig', [
+            'instruction' => $instruction,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_instruction_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Instruction $instruction, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(InstructionType::class, $instruction);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_instruction_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('instruction/edit.html.twig', [
+            'instruction' => $instruction,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_instruction_delete', methods: ['POST'])]
+    public function delete(Request $request, Instruction $instruction, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$instruction->id, $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($instruction);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_instruction_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/InstructionType.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Entity\Instruction;
+use App\Entity\Privilege;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @template-extends AbstractType<Instruction>
+ */
+class InstructionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('privileges', EntityType::class, [
+                'class' => Privilege::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Instruction::class,
+        ]);
+    }
+}
