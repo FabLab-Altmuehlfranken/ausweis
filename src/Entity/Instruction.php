81dedc74a1880ea36f59acafadbe75b8f5a28b14
Bernd Stellwag <burned@zerties.org>
2026-09-22 23:53:25 +0200

    use make:entity to add instructions

---
+++ src/Entity/Instruction.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\InstructionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InstructionRepository::class)]
+#[ORM\UniqueConstraint(fields: ['name'])]
+class Instruction
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    public private(set) int $id;
+
+    #[ORM\Column(length: 255)]
+    public private(set) string $name;
+
+    /**
+     * @var Collection<int, Privilege>
+     */
+    #[ORM\ManyToMany(targetEntity: Privilege::class, inversedBy: 'instructions')]
+    public private(set) Collection $privileges;
+
+    public function __construct()
+    {
+        $this->privileges = new ArrayCollection();
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
